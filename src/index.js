4e04f83c37b40c1d6ebe002e6ae60aa53b8752f3
Nguyen Hai Long <longkool246@gmail.com>
2024-03-24 11:38:50 +0700

    update product

---
+++ src/index.js
@@ -0,0 +1,27 @@
+//Hamburger_Menu
+const headerBars = document.querySelector('.header__bars')
+const overlay = document.querySelector('.header__overlay')
+const navigation = document.querySelector('.navigation')
+const scrollHeader = document.querySelector('.header')
+
+headerBars.addEventListener('click', () => {
+  overlay.classList.toggle('show-overlay')
+  navigation.classList.toggle('show-menu')
+})
+
+overlay.addEventListener('click', () => {
+  overlay.classList.remove('show-overlay')
+  navigation.classList.remove('show-menu')
+})
+
+// Sticky_Menu
+window.addEventListener("scroll", function() {
+  var header = document.querySelector(".header");
+  if (window.scrollY > 0) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+});
+
+//scroll_navigation
