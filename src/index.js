84e096f4d3df5b92b71d754c901c52b3c327e2a2
Nguyen Hai Long <longkool246@gmail.com>
2024-03-24 18:01:05 +0700

    Login-signup

--- src/index.js
+++ src/index.js
@@ -1,27 +1,39 @@
-//Hamburger_Menu
-const headerBars = document.querySelector('.header__bars')
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+const test = document.querySelectorAll('.test')
+
+
+registerLink.addEventListener('click' , ()=> {
+    wrapper.classList.add('active');
+    test.forEach(element => {
+        element.classList.add('active')
+    });
+});
+
+loginLink.addEventListener('click' , ()=> {
+    wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click' , ()=> {
+    wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click' , ()=> {
+    wrapper.classList.remove('active-popup');
+});
+
+//*//
+const headerBars = document.querySelector('.btnLogin-popup')
 const overlay = document.querySelector('.header__overlay')
-const navigation = document.querySelector('.navigation')
-const scrollHeader = document.querySelector('.header')
 
 headerBars.addEventListener('click', () => {
   overlay.classList.toggle('show-overlay')
-  navigation.classList.toggle('show-menu')
 })
 
-overlay.addEventListener('click', () => {
+iconClose.addEventListener('click', () => {
   overlay.classList.remove('show-overlay')
-  navigation.classList.remove('show-menu')
+  headerRight.classList.remove('show-menu')
 })
-
-// Sticky_Menu
-window.addEventListener("scroll", function() {
-  var header = document.querySelector(".header");
-  if (window.scrollY > 0) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-});
-
-//scroll_navigation
