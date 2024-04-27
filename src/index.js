40a6ecc974531a88997bab432e3492bf49a4ba8f
Nguyen Hai Long <longkool246@gmail.com>
2024-04-27 12:54:05 +0700

    out

--- src/index.js
+++ src/index.js
@@ -1,29 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const mbHeaderLeft = document.querySelector('.mb-header-left');
-  const navigation = document.querySelector('.navigation');
+document.addEventListener("DOMContentLoaded", function () {
+  const mbHeaderLeft = document.querySelector(".mb-header-left");
+  const navigation = document.querySelector(".navigation");
 
-  // Thêm sự kiện click vào mb-header-left
-  mbHeaderLeft.addEventListener('click', function() {
-    // Thay đổi thuộc tính right của .navigation từ 120% sang 20%
-    navigation.style.right = '20%';
+  mbHeaderLeft.addEventListener("click", function () {
+    navigation.style.right = "20%";
   });
 
-  // Lấy phần tử closeMb trong phạm vi DOMContentLoaded để sử dụng
-  const closeMb = document.querySelector('.navigation-header__close');
-  
-  // Kiểm tra xem closeMb có tồn tại không trước khi gắn sự kiện
+  const closeMb = document.querySelector(".navigation-header__close");
+
   if (closeMb) {
-    closeMb.addEventListener('click', function() {
-      // Thay đổi thuộc tính right của .navigation từ 20% về 120%
-      navigation.style.right = '120%';
+    closeMb.addEventListener("click", function () {
+      navigation.style.right = "120%";
     });
   } else {
-    console.error('Không tìm thấy phần tử .navigation-header__close trong DOM.');
+    console.error(
+      "Không tìm thấy phần tử .navigation-header__close trong DOM."
+    );
   }
 });
 
 // Sticky_Menu
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
   var header = document.querySelector(".header");
   if (window.scrollY > 0) {
     header.classList.add("sticky");
@@ -33,7 +30,7 @@ window.addEventListener("scroll", function() {
 });
 
 // Sticky_Menu_mobile
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
   var headermobi = document.querySelector(".header-mobile");
   if (window.scrollY > 0) {
     headermoni.classList.add("sticky");
@@ -42,69 +39,67 @@ window.addEventListener("scroll", function() {
   }
 });
 
-const wrapper = document.querySelector('.wrapper');
-const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.register-link');
-const btnPopup = document.querySelector('.btnLogin-popup');
-const iconClose = document.querySelector('.icon-close');
-const test = document.querySelectorAll('.test')
+const wrapper = document.querySelector(".wrapper");
+const loginLink = document.querySelector(".login-link");
+const registerLink = document.querySelector(".register-link");
+const btnPopup = document.querySelector(".btnLogin-popup");
+const iconClose = document.querySelector(".icon-close");
+const test = document.querySelectorAll(".test");
 
-
-registerLink.addEventListener('click' , ()=> {
-    wrapper.classList.add('active');
-    test.forEach(element => {
-        element.classList.add('active')
-    });
+registerLink.addEventListener("click", () => {
+  wrapper.classList.add("active");
+  test.forEach((element) => {
+    element.classList.add("active");
+  });
 });
 
-loginLink.addEventListener('click' , ()=> {
-    wrapper.classList.remove('active');
+loginLink.addEventListener("click", () => {
+  wrapper.classList.remove("active");
 });
 
-btnPopup.addEventListener('click' , ()=> {
-    wrapper.classList.add('active-popup');
+btnPopup.addEventListener("click", () => {
+  wrapper.classList.add("active-popup");
 });
 
-iconClose.addEventListener('click' , ()=> {
-    wrapper.classList.remove('active-popup');
+iconClose.addEventListener("click", () => {
+  wrapper.classList.remove("active-popup");
 });
 
 //*//
-const headerBars = document.querySelector('.btnLogin-popup')
-const overlay = document.querySelector('.header__overlay')
+const headerBars = document.querySelector(".btnLogin-popup");
+const overlay = document.querySelector(".header__overlay");
 
-headerBars.addEventListener('click', () => {
-  overlay.classList.toggle('show-overlay')
-})
+headerBars.addEventListener("click", () => {
+  overlay.classList.toggle("show-overlay");
+});
 
-iconClose.addEventListener('click', () => {
-  overlay.classList.remove('show-overlay')
-  headerRight.classList.remove('show-menu')
-})
+iconClose.addEventListener("click", () => {
+  overlay.classList.remove("show-overlay");
+  headerRight.classList.remove("show-menu");
+});
 
 //*//
-const myButton = document.getElementById('myButton');
+const myButton = document.getElementById("myButton");
 
-myButton.addEventListener('click', function() {
-
-  window.location.href = '/index.html';
+myButton.addEventListener("click", function () {
+  window.location.href = "/index.html";
 });
 
 //*//
 
-document.addEventListener('DOMContentLoaded', function() {
-  const numberInput = document.querySelector('.number-input');
-  const decrementButton = document.querySelector('.decrement');
-  const incrementButton = document.querySelector('.increment');
+document.addEventListener("DOMContentLoaded", function () {
+  const numberInput = document.querySelector(".number-input");
+  const decrementButton = document.querySelector(".decrement");
+  const incrementButton = document.querySelector(".increment");
 
-  decrementButton.addEventListener('click', function() {
+  decrementButton.addEventListener("click", function () {
     const currentValue = parseInt(numberInput.value) || 0;
     if (currentValue > numberInput.min) {
       numberInput.value = currentValue - 1;
     }
   });
 
-  incrementButton.addEventListener('click', function() {
+  incrementButton.addEventListener("click", function () {
     const currentValue = parseInt(numberInput.value) || 0;
     if (currentValue < numberInput.max) {
       numberInput.value = currentValue + 1;
