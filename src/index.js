7d986e71835a6e209afb9a6f91b180f48a58ad97
Nguyen Hai Long <longkool246@gmail.com>
2024-04-27 08:48:17 +0700

    mobile nav

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const mbHeaderLeft = document.querySelector('.mb-header-left');
+  const navigation = document.querySelector('.navigation');
+
+  // Thêm sự kiện click vào mb-header-left
+  mbHeaderLeft.addEventListener('click', function() {
+    // Thay đổi thuộc tính right của .navigation từ 120% sang 20%
+    navigation.style.right = '20%';
+  });
+
+  // Lấy phần tử closeMb trong phạm vi DOMContentLoaded để sử dụng
+  const closeMb = document.querySelector('.navigation-header__close');
+  
+  // Kiểm tra xem closeMb có tồn tại không trước khi gắn sự kiện
+  if (closeMb) {
+    closeMb.addEventListener('click', function() {
+      // Thay đổi thuộc tính right của .navigation từ 20% về 120%
+      navigation.style.right = '120%';
+    });
+  } else {
+    console.error('Không tìm thấy phần tử .navigation-header__close trong DOM.');
+  }
+});
+
+
+
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
@@ -37,3 +63,33 @@ iconClose.addEventListener('click', () => {
   overlay.classList.remove('show-overlay')
   headerRight.classList.remove('show-menu')
 })
+
+//*//
+const myButton = document.getElementById('myButton');
+
+myButton.addEventListener('click', function() {
+
+  window.location.href = '/index.html';
+});
+
+//*//
+
+document.addEventListener('DOMContentLoaded', function() {
+  const numberInput = document.querySelector('.number-input');
+  const decrementButton = document.querySelector('.decrement');
+  const incrementButton = document.querySelector('.increment');
+
+  decrementButton.addEventListener('click', function() {
+    const currentValue = parseInt(numberInput.value) || 0;
+    if (currentValue > numberInput.min) {
+      numberInput.value = currentValue - 1;
+    }
+  });
+
+  incrementButton.addEventListener('click', function() {
+    const currentValue = parseInt(numberInput.value) || 0;
+    if (currentValue < numberInput.max) {
+      numberInput.value = currentValue + 1;
+    }
+  });
+});
